c2083de0ca08049fda005c4c12b2fd07f4efbe68
Joshua Ekwonye <88485251+iamjooshua@users.noreply.github.com>
2022-06-24 12:02:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("My name is Joshua Ekwonye")
+alert("My height is 182cm")
+alert("I am from Nigeria")
+
+// Alternative...
+console.log("My name is Joshua Ekwonye")
+console.log("My height is 182cm")
+console.log("I am from Nigeria")
